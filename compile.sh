b48ae2e6911560fdfd73175d4880eec3f611adb6
Brian Schlenker <bschlenk@umich.edu>
2017-02-05 12:57:06 -0800

    Add output_wrapper to compilation

--- compile.sh
+++ compile.sh
@@ -3,11 +3,16 @@
 OUTPUT='bundle.js'
 SOURCE_MAP="$OUTPUT.map"
 
+# --debug \
+# --formatting=PRETTY_PRINT \
+# --formatting=PRINT_INPUT_DELIMITER
 # --compilation_level ADVANCED \
 closure-compiler \
+    --language_in ECMASCRIPT6_STRICT \
     --warning_level VERBOSE \
+    --assume_function_wrapper true \
+    --output_wrapper "(function() {%output%})();\n\n//# sourceMappingURL=/$SOURCE_MAP" \
     --js 'src/*.js' \
     --js_output_file $OUTPUT \
     --create_source_map $SOURCE_MAP
 
-echo "\n//# sourceMappingURL=/$SOURCE_MAP" >> "./$OUTPUT"
