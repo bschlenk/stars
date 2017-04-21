a333d54d3856bda0a04cda883f5441b25beb12f9
Brian Schlenker <bschlenk@umich.edu>
2017-04-21 02:25:02 -0700

    Add unit test for color.js

---
+++ test/color.js
@@ -0,0 +1,43 @@
+import Color from '../src/color';
+import expect from 'expect';
+
+describe('Color', () => {
+  describe('#fromString()', () => {
+    it('should parse a regular hex string', () => {
+      const color = Color.fromString('#1234AB');
+      expect(color).toEqual(new Color(18, 52, 171));
+    });
+
+    it('should parse a lowercase hex string', () => {
+      const color = Color.fromString('#1234ab');
+      expect(color).toEqual(new Color(18, 52, 171));
+    });
+
+    it('should parse a short hex string', () => {
+      const color = Color.fromString('#AB2');
+      expect(color).toEqual(new Color(170, 187, 34));
+    });
+
+    it('should parse a lowercase short hex string', () => {
+      const color = Color.fromString('#ab2');
+      expect(color).toEqual(new Color(170, 187, 34));
+    });
+
+    it('should parse an rgb color string', () => {
+      const color = Color.fromString('rgb(18, 52, 171)');
+      expect(color).toEqual(new Color(18, 52, 171));
+    });
+
+    it('should parse an rgba color string', () => {
+      const color = Color.fromString('rgba(18, 52, 171, 0.2)');
+      expect(color).toEqual(new Color(18, 52, 171, 0.2));
+    });
+
+    it('should return null for invalid strings', () => {
+      expect(Color.fromString('abc')).toNotExist();
+      expect(Color.fromString('invalid')).toNotExist();
+      expect(Color.fromString('rgb()')).toNotExist();
+      expect(Color.fromString('rgb(1, 2, E)')).toNotExist();
+    });
+  });
+});
